3fbff05136ea7e935bcd42526bfe4a03c32d948e
Koen Serry <482723+koen-serry@users.noreply.github.com>
2025-03-14 16:29:25 +0100

    Corrected version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def get_long_description():
 
 setup(
     name="twikey-api-python",
-    version="v0.2.2",
+    version="v0.2.3",
     description="Python interface with the Twikey api",
     author="Twikey",
     author_email="support@twikey.com",
